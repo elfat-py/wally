54e6eaa0777c4e35b904f6bb8279e173a59edf59
elfat-pt <elfatmemaj@gmail.com>
2024-01-09 21:07:16 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wally
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButton = (RadioButton)sender;
+
+            // Hide all ListBoxes
+            ListBoxLibrary.Visibility = Visibility.Collapsed;
+            ListBoxStore.Visibility = Visibility.Collapsed;
+
+            // Show the specific ListBox based on the selected RadioButton
+            if (radioButton.Content.ToString() == "Library")
+            {
+                ListBoxLibrary.Visibility = Visibility.Visible;
+            }
+            else if (radioButton.Content.ToString() == "Store")
+            {
+                ListBoxStore.Visibility = Visibility.Visible;
+            }
+
+        }
+
+       
+    }
+    
+}
